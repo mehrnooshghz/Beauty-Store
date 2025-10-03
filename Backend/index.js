b11d55d6152310f23e0e2006252f4abecfacb7d9
mehrnooshghz <mehrnooshghz00@gmail.com>
2025-10-03 15:48:31 +0330

    working on backend

---
+++ Backend/index.js
@@ -0,0 +1,12 @@
+import app from "./app.js"
+import dotenv from "dotenv";
+import dbConnection from "./util/db.js";
+dotenv.config();
+
+
+// server 
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    dbConnection();
+})
