d863ab180c20c7c1ebea8a54220059bdbcdc08e8
mehrnooshghz <mehrnooshghz00@gmail.com>
2025-10-09 20:23:28 +0330

    worked on product model and controller

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,39 +1,54 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    address: {
+      type: String,
+    },
 
-const userSchema = mongoose.Schema({
-     
-     name:{
-        type: String,
-        require: true
-     },
-     email:{
-        type: String,
-        require: true,
-     },
-     password:{
-        type: String,
-        require:true
-     },
-     address:{
-        type: String,
-     },
+    phone: {
+      type: String,
+    },
 
-     phone:{
-        type: String,
-     },
+    role: {
+      type: String,
+      default: "user",
+    },
 
-     role:{
-        type: String,
-        default:"user"
-     },
+    status: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-     status:{
-        type:Number,
-        default:0
-     },
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
 
-})
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
+userSchema.methods.matchPassword = async function(enteredPassword){
+  return await bcrypt.compare(enteredPassword, this.password);  
+}
 
 const User = mongoose.model("User", userSchema);
 export default User;
