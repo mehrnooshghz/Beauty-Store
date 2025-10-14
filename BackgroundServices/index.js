0d53df34f7ba2b1a9607ebce48298bfadb22e7d2
mehrnooshghz <mehrnooshghz00@gmail.com>
2025-10-14 20:04:21 +0330

    finish working on bservices

--- BackgroundServices/index.js
+++ BackgroundServices/index.js
@@ -3,6 +3,10 @@ import dotenv from "dotenv";
 import dbConnection from "./utils/db.js";
 import cron from "node-cron";
 import sendWelcomeEmail from "./EmailServices/sendWelcomeEmail.js";
+import sendPendingOrderEmail from "./EmailServices/sendPendingOrderEmail.js";
+import sendDeliveredOrderEmail from "./EmailServices/sendDeliveredOrderEmail.js";
+import sendPromotionEmail from "./EmailServices/sendPromotionemail.js";
+
 
 dotenv.config();
 const app = express();
@@ -13,11 +17,21 @@ const services = () => {
   // Run every 10 seconds to check for new timetable requests
   cron.schedule("*/10 * * * * *", () => {
     sendWelcomeEmail();
-   
+    sendPendingOrderEmail();
+    sendDeliveredOrderEmail();
     
      // Add timetable email service
   });
 };
+
+const promotion = () => {
+  cron.schedule("30 5 * * 5", () => {
+    sendPromotionEmail();
+  });
+};
+
+services();
+promotion();
 
 app.listen(PORT, () => {
   console.log(`Background services is running on port ${PORT}`);
