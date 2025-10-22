45d1893c72bb5b06a800eb6fe382786a187d59cf
mehrnooshghz <mehrnooshghz00@gmail.com>
2025-10-22 10:33:56 +0330

    started frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,51 @@
+import Home from "./pages/Home";
+import {RouterProvider, createBrowserRouter, Outlet} from "react-router-dom";
+import Cart from "./pages/Cart";
+import Myaccount from "./pages/Myaccount";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Announcement from "./components/Announcement";
+
+function App() {
+
+   const Layout = () => {
+    return (
+        <div>
+          <Announcement />
+          <Navbar />
+          <Outlet />
+          <Footer />
+        </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+       {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/cart",
+          element: <Cart />,
+        },
+         {
+          path: "/myaccount",
+          element: <Myaccount />
+        } 
+    ],
+   },
+ ])
+
+
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
