35e9ce6aaf2012b47f0d451741631292e6b40ebc
mehrnooshghz <mehrnooshghz00@gmail.com>
2025-10-24 10:35:56 +0330

    worked on product detailed page

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -2,13 +2,10 @@ import Product from "./Product";
 const Products = () => {
   return (
     <div className="flex flex-wrap mx-[40px]">
-       <Product />
-       <Product />
-       <Product />
-       <Product />
-       
-       
-      
+       <Product img="/lotion.jpg"/>
+       <Product img="/lotion1.jpg"/>
+       <Product img="/lotion2.jpg"/>
+       <Product img="/serum1.jpg"/>
     </div>
   );
 }
